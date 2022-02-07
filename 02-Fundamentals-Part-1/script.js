9d743331bd1589a1233112987e5c687e61d8d9c8
Aakash Rao <aakash.codes02@gmail.com>
2022-02-07 13:36:32 +0530

    data type completed

--- 02-Fundamentals-Part-1/script.js
+++ 02-Fundamentals-Part-1/script.js
@@ -5,15 +5,16 @@ console.log(40 + 20 + 1);
 */
 
 //=============VALUES & VARIABLES=============
+/*
 console.log("Aakash");
 console.log(18);
 
 let firstName = "Bob"; // Variable declaration
 console.log(firstName);
 
+* Variable name Conventions
 let $function = 18;
 let aakash_rao = "AR";
-
 let person = "Aakash";
 let PI = 3.1415;
 
@@ -22,3 +23,42 @@ let myCurrentJob = "Teacher";
 
 let job1 = "programmer";
 let job2 = "teacher";
+
+console.log(myFirstJob);
+*/
+
+//============= DATA TYPES =============
+/*
+? In JavaScript every value is either object or a Primitive Value.
+
+* THE SEVEN PRIMITIVE DATA TYPES
+- Number
+- Strings
+- Boolean
+- Undefined
+- Null
+- Symbol
+- BigInt
+
+let javascriptisFun = true;
+console.log(javascriptisFun);
+
+// console.log(typeof javascriptisFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javascriptisFun = "YES!";
+console.log(typeof javascriptisFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(null);
+console.log(typeof null);
+*/
+
+//============= LET, CONST & VAR =============
