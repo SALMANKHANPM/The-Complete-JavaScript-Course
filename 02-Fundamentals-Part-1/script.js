a57e3a0e4fd985832290014d6ba449a14e3962f0
Aakash Rao <aakash.codes02@gmail.com>
2022-02-07 12:50:54 +0530

    CSS File deleted

--- 02-Fundamentals-Part-1/script.js
+++ 02-Fundamentals-Part-1/script.js
@@ -1,4 +1,24 @@
+/*
 let js = "amazing";
-// if (js === "amazing") alert("JavaScript is FUN!!");
-
+if (js === "amazing") alert("JavaScript is FUN!!");
 console.log(40 + 20 + 1);
+*/
+
+//=============VALUES & VARIABLES=============
+console.log("Aakash");
+console.log(18);
+
+let firstName = "Bob"; // Variable declaration
+console.log(firstName);
+
+let $function = 18;
+let aakash_rao = "AR";
+
+let person = "Aakash";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
