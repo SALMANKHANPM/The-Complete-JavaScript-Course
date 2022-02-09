1ffc9fd53dbb3582c9687a2d0b5b407bccbe1a5b
Aakash Rao <aakash.codes02@gmail.com>
2022-02-09 19:17:50 +0530

    Intro to arrays

--- 03-Fundamentals-Part-2/script.js
+++ 03-Fundamentals-Part-2/script.js
@@ -106,3 +106,39 @@ const yearUntilRetirement = function (birthYear, firstName) {
 console.log(yearUntilRetirement(2003, "Aakash"));
 console.log(yearUntilRetirement(1970, "Mike"));
 */
+
+//================== INTRO TO ARRAYS:- ==================
+/*
+const friend1 = "Micheal";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Micheal", "Steven", "Peter"];
+console.log(friends);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]); // .length is not based so when we subtract it with -1 we get the last element of the array.
+
+friends[2] = "Jay";
+console.log(friends);
+
+const firstName = "Aakash";
+const aakash = [firstName, "Rao", 2037 - 1991, "student", friends];
+console.log(aakash);
+
+const calcAge = function (birthYear) {
+  return 2022 - birthYear;
+};
+
+const years = [1996, 1967, 2000, 2003, 2008];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+*/
