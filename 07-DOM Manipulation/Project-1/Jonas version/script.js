8baf3433320e9f20bc8d9bc9399a2cde00e13f61
Aakash Rao <aakash.codes02@gmail.com>
2022-02-12 14:48:06 +0530

    Project-#1 Guess my number with again btn but without refactoring

--- 07-DOM Manipulation/Project-1/Jonas version/script.js
+++ 07-DOM Manipulation/Project-1/Jonas version/script.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -14,6 +13,8 @@ document.querySelector('.check').addEventListener('click', function () {
   //  When player wins
   else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
+
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
   }
@@ -39,4 +40,16 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log(secretNumber);
+  score = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
