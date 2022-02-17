b1c763ca1028c49cfcd2d52c67ba3aaf32685236
Aakash Rao <aakash.codes02@gmail.com>
2022-02-17 13:27:17 +0530

    Spread & rest operator done

--- 09-Modern Operators (ES6)/script.js
+++ 09-Modern Operators (ES6)/script.js
@@ -142,3 +142,196 @@ const {
 } = openingHours;
 console.log(o, c);
 */
+
+//============ THE SPREAD OPERATOR:- ============
+/*
+// Take all the values out of the "arr" array and write them individually.
+// 👉🏼 Use Case 👈🏼
+// 👉🏼 Array literal
+// 👉🏼 When we pass arguments to the function
+// 👉🏼 Expand the array & adding a new value in that array.
+
+const arr = [7, 8, 9];
+const badNewArray = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArray);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+console.log(...newArr);
+
+const restaurant = {
+  name: 'Classico Italino',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  startMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  // Object inside an object
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.startMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  // Passing an object to a function as a Parameter
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order Recieved!! ${this.startMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliverd to ${address} at ${time}.`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your deleicious pasta with ${ing1}, ${ing2} & ${ing3}.`
+    );
+  },
+};
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menu = [...restaurant.mainMenu, ...restaurant.startMenu];
+console.log(menu);
+
+// Iterables are arrays -- strings -- maps -- sets but not objects
+const str = 'Aakash';
+const letters = [...str, ' ', 'R.'];
+console.log(letters);
+console.log(...str);
+
+// Taking the valuse from the prompt window and passing to to the orderpasta method on restaurant object.
+const ingredients = [
+  prompt(`Let's make pasta! Ingredient 1?`),
+  prompt(`Let's make pasta! Ingredient 2?`),
+  prompt(`Let's make pasta! Ingredient 3?`),
+];
+console.log(ingredients);
+
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurant = { foundedIn: 2003, ...restaurant, founder: 'Aakash' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurant.name);
+console.log(restaurantCopy.name);
+
+
+//============ THE REST PATTERN & PARAMETERS:- ============
+// takes the rest of the elements  & put them into new Array.
+// Conditions for rest parameters:-
+// 🤔 Should be last in any destructing assignment otherwise how does one know that it has to select rest of the last parameters.
+// And that's the reason there should be only one rest parameter in any destructuring assignment.
+
+const restaurant = {
+  name: 'Classico Italino',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  startMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+
+  // Object inside an object
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.startMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  // Passing an object to a function as a Parameter
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order Recieved!! ${this.startMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliverd to ${address} at ${time}.`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your deleicious pasta with ${ing1}, ${ing2} & ${ing3}.`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+// 1. Destucturing
+// SPREAD BECAUSE ON THE RIGHT SIDE OF ASSIGNMENT OPERATOR
+const arr = [1, 2, ...[3, 4]];
+
+// REST BECAUSE ON THE LEFT SIDE OF ASSIGNMENT OPERATOR
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , rissoto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.startMenu,
+];
+console.log(pizza, rissoto, otherFood);
+
+// Let work on objects
+const { sat, ...weekDays } = restaurant.openingHours;
+console.log(weekDays);
+
+// 2. Functions
+const add = function (...numbers) {
+  // packs the value again
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x); // unpack the values
+
+restaurant.orderPizza('mushrooms', 'onions', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
+*/
