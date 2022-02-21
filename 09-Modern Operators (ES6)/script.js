02ef92589b457a32e784ecc608d026cf5b213ec9
Aakash Rao <aakash.codes02@gmail.com>
2022-02-21 11:29:54 +0530

    Learned about Map Fundamentals

--- 09-Modern Operators (ES6)/script.js
+++ 09-Modern Operators (ES6)/script.js
@@ -823,3 +823,37 @@ console.log(
 );
 console.log(new Set('Aakash Rao').size);
 */
+
+//============ MAPS: FUNDAMENTALS ============
+/*
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+rest.set(2, 'Lisbon, Portugal');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+// to read data from a map we use get method
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+const time = 7;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+rest.set(document.querySelector('h1'), 'Heading');
+const arr = [1, 2];
+rest.set(arr, 'Test');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+*/
