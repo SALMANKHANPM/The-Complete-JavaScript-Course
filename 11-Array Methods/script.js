cfe643ec8788c7edf4b3b16a8182fc40f0cfbe92
Aakash Rao <aakash.codes02@gmail.com>
2022-03-02 00:28:32 +0530

    Simple array Methods

---
+++ 11-Array Methods/script.js
@@ -0,0 +1,115 @@
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
+/*
+
+// ============ SIMPLE ARRAY METHODS ============
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(arr.slice(3)); // start from 3 uptil the end
+console.log(arr.slice(2, 4)); // start from 2 uptil 4
+console.log(arr.slice(-2)); // only slice the last element
+console.log(arr.slice(-1)); // only slice the last element
+console.log(arr.slice(1, -2)); // only slice the last element
+console.log(arr.slice()); // creates a shallow copy
+console.log([...arr]); // creates a shallow copy
+
+// SPLICE METHOD
+// console.log(arr.splice(3));
+arr.splice(-1); //  mutate & delete the original array elements & return the deleted array elements
+console.log(arr);
+arr.splice(1, 2);
+// 1st parameter = where to start deleting
+// 2nd parameter = how much elements to delete
+console.log(arr);
+
+// REVERSE METHOD
+arr = ['a', 'b', 'c', 'd', 'e'];
+
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse()); // mutates the array & return the reverse array of elements
+console.log(arr2);
+
+// CONCAT
+const letters = arr.concat(arr2); // concat & returns a new array
+console.log(letters);
+console.log([...arr, ...arr2]); // concat & returns a new array but using spread operator
+
+// JOIN METHOD
+console.log(letters.join(' - '));
+*/
