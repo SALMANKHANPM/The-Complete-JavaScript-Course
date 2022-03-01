04f1261c8c67636fd51933ef56c064eb989fc9dd
Aakash Rao <aakash.codes02@gmail.com>
2022-03-01 21:55:25 +0530

    Closures finally done

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -282,3 +282,49 @@ runOnce();
 
 (() => console.log('This will also never run again'))();
 */
+
+//============ MORE ABOUT CLOSURES:- ============
+/*
+// ===== EXAMPLE-1 =====
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Reassigning f function
+h();
+console.dir(f);
+f();
+
+// ===== EXAMPLE-2 =====
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+*/
