8cf5dfe2db136be08b475b6d1d36f5ac46cc757e
Aakash Rao <aakash.codes02@gmail.com>
2022-03-08 08:08:53 +0530

    ForEach method done

--- 11-Array Methods/script.js
+++ 11-Array Methods/script.js
@@ -118,20 +118,20 @@ console.log(letters.join(' - '));
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
   if (movement > 0) {
-    console.log(`You deposited ${movement}`);
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
   } else {
-    console.log(`You withdrew ${Math.abs(movement)}`);
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
   }
 }
 
 console.log('----FOR EACH METHOD----');
-movements.forEach(function (movement) {
+movements.forEach(function (movement, index, array) {
   if (movement > 0) {
-    console.log(`You deposited ${movement}`);
+    console.log(`Movement ${index + 1}: You deposited ${movement}`);
   } else {
-    console.log(`You withdrew ${Math.abs(movement)}`);
+    console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
   }
 });
 // 0: fucntion(200)
