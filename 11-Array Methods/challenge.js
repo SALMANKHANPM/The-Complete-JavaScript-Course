b425da9cead57c83894a5c0bfec46992018155bf
Aakash Rao <aakash.codes02@gmail.com>
2022-03-18 11:17:56 +0530

    Array Methods Coding Challenge 3 done

--- 11-Array Methods/challenge.js
+++ 11-Array Methods/challenge.js
@@ -47,9 +47,7 @@ checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 */
 
 /*
-
-// =============== CODING CHALLENGE - 01 ===============
-
+// =============== CODING CHALLENGE - 02 ===============
 
 Let's go back to Julia and Kate's study about dogs. This time, they want to convert
 dog ages to human ages and calculate the average age of the dogs in their study.
@@ -111,5 +109,30 @@ const calcAverageHumanAge = function (ages) {
 const average1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const average2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 
+console.log(average1, average2);
+*/
+
+// =============== CODING CHALLENGE - 03 ===============
+
+/*
+Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time as an arrow function, and using chaining!
+
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK
+
+
+const calcAverageHumanAge = ages => {
+  const average = ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+  return average;
+};
+
+const average1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const average2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(average1, average2);
 */
