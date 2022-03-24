7e383a0e3bbccc695c79a3a1faf349a6e7fac94a
Aakash Rao <aakash.codes02@gmail.com>
2022-03-24 05:58:57 +0530

    Array Methods Sorting Arrays using sort method method

--- 11-Array Methods/lecture.js
+++ 11-Array Methods/lecture.js
@@ -122,6 +122,7 @@ console.log(movements.every(mov => mov > 0));
 console.log(account4.movements.every(mov => mov > 0));
 */
 
+/*
 // THE FLAT & FLATMAP METHOD
 const nested = [[1, 2, 3], [4, 5, 6], 7, 8];
 console.log(nested.flat());
@@ -141,3 +142,36 @@ const overalBalance2 = accounts
   .flatMap(acc => acc.movements)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overalBalance2);
+*/
+
+/*
+// SORTING ARRAYS
+// Strings
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort());
+console.log(owners);
+
+// numbers
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// console.log(movements);
+
+// return < 0, A, B (keep order)
+// return > 0 B, A (switch order)
+
+// Ascending
+// movements.sort((a, b) => {
+//   if (a > b) return 1; // a - b === +ve
+//   if (a < b) return -1; // a - b === -ve
+// });
+
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+// Descending
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (a < b) return 1;
+// });
+movements.sort((a, b) => b - a);
+console.log(movements);
+*/
