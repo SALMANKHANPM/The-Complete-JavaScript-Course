eee1be26eea9d4c77d2776b743ced2b016fe5465
Aakash Rao <aakash.codes02@gmail.com>
2022-04-06 22:54:21 +0530

    Math & Rounding

---
+++ 12-Numbers Dates Timers/lecture.js
@@ -0,0 +1,76 @@
+/////////////////////////////////////////////////
+//////////////////////////////////////////////
+// Coverting & Checking Numbers
+/*
+// Base 10 - 0 to 9, 1/10 = 0.1  3/10 = 3.33333
+
+// Binary base 2 - 0 1
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
+
+// Converting
+console.log(Number('23'));
+console.log(+'23');
+
+// Parsing
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('e34', 10));
+
+console.log(Number.parseInt('30.5px'));
+console.log(Number.parseFloat('   30.5px   '));
+
+// console.log(parseFloat('   30.5px   '));
+console.log(Number.isNaN(28));
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(+'20x'));
+console.log(Number.isNaN(23 / 0));
+
+//  Checking if a value is a number
+console.log(Number.isFinite(23));
+console.log(Number.isFinite('23'));
+console.log(Number.isFinite(+'23x'));
+console.log(Number.isFinite(23 / 0));
+*/
+
+// Math & Rounding
+
+/*
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2));
+
+console.log(Math.max(5, 18, '23', 11, 2));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+// 0..1 -> 0....(max-min) -> min...(max-min + min)
+console.log(randomInt(10, 20));
+
+// Rounding Integers
+console.log(Math.trunc(23.3));
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor('23.9'));
+
+console.log(Math.floor(-23.9));
+console.log(Math.trunc(-23.9));
+
+// Rounding Decimals
+console.log((2.3).toFixed(0));
+console.log((2.3).toFixed(3));
+console.log(+(2.345).toFixed(2));
+*/
