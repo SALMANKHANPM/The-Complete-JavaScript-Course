f44452e0442acf076d3cb468160df3b61e76f4e0
Aakash Rao <aakash.codes02@gmail.com>
2022-04-07 13:59:03 +0530

    All about Dates

--- 12-Numbers Dates Timers/lecture.js
+++ 12-Numbers Dates Timers/lecture.js
@@ -123,3 +123,42 @@ console.log(huge + ' is REALLY BIG');
 // Divisions
 console.log(10n / 3n);
 */
+
+// CREATING DATES
+/*
+// First way
+const now = new Date();
+console.log(now);
+
+// Second way
+console.log(new Date('Apr 07 2022 08:14:03'));
+console.log(new Date('December 24 2017'));
+
+console.log(account1);
+console.log(new Date(account1.movementsDates[0]));
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 33));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+
+// working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142237180000));
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
+*/
