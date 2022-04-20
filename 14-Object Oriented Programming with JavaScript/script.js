b5bb30e23266c495ea0aa3758b420597d61cd866
Aakash Rao <aakash.codes02@gmail.com>
2022-04-20 21:21:54 +0530

    Coding Challenge 02

--- 14-Object Oriented Programming with JavaScript/script.js
+++ 14-Object Oriented Programming with JavaScript/script.js
@@ -310,3 +310,48 @@ const Sarah = Object.create(PersonProto);
 Sarah.init('Sarah', 2000);
 Sarah.calcAge();
 */
+
+// ================== CODING CHALLENGE 02 ==================
+
+/*
+
+Your tasks:
+
+1. Re-create Challenge #1, but this time using an ES6 class (call it 'CarCl')
+
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide
+by 1.6)
+
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but
+converts it to km/h before storing the value, by multiplying the input by 1.6)
+
+4. Create a new car and experiment with the 'accelerate' and 'brake'
+methods, and with the getter and setter.
+
+Test data:
+§ Data car 1: 'Ford' going at 120 km/h
+
+
+GOOD LUCK 😀
+
+*/
+
+class Carcl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(value) {
+    this.speed = value * 1.6;
+  }
+}
+
+const BMW = new Carcl('BMW', 120);
+console.log(BMW);
+
+console.log(BMW.speedUS);
