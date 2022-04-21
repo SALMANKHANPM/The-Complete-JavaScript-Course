284d2085ed3b4bffa7036d737b4fc46e8b7ff61c
Aakash Rao <aakash.codes02@gmail.com>
2022-04-21 22:27:15 +0530

    Encapsulation: Protected Properties & Mehthods

--- 14-Object Oriented Programming with JavaScript/script.js
+++ 14-Object Oriented Programming with JavaScript/script.js
@@ -603,3 +603,59 @@ acc1.requestLoan(10000);
 
 console.log(acc1.pin);
 */
+
+// ================= ENCAPSULATION: PROTECTED PROPERTIES & METHODS =====================
+/*
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this._pin = pin;
+    // PROTECTED PROPERTY
+    this._movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account with us, ${this.owner} :)`);
+  }
+
+  // PUBLIC INTERFACE
+
+  getMovements() {
+    return this._movements;
+  }
+
+  deposit(val) {
+    this._movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  _approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Your loan has been approved!`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1234, []);
+
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
+
+acc1.deposit(250);
+acc1.withdraw(-140);
+console.log(acc1);
+acc1.requestLoan(10000);
+
+console.log(acc1);
+console.log(acc1._pin);
+
+console.log(acc1.getMovements());
+*/
