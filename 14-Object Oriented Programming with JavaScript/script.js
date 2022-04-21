9587ce6c39388494adb1a7a9658243858cac8c3f
Aakash Rao <aakash.codes02@gmail.com>
2022-04-21 23:56:15 +0530

    Coding Challenge 04 Done

--- 14-Object Oriented Programming with JavaScript/script.js
+++ 14-Object Oriented Programming with JavaScript/script.js
@@ -804,3 +804,67 @@ acc1.deposit(250).deposit(350).deposit(450).requestLoan(2400).withdraw(4000);
 
 console.log(acc1.getMovements());
 */
+
+// =============== CODING CHALLENGE: 04 =====================
+
+/*
+Your tasks:
+
+1. Re-create Challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class.
+
+2. Make the 'charge' property private
+
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. Then experiment with chaining!
+
+Test data:
+§ Data car 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK
+
+
+class Carcl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  acclerate(speed) {
+    this.speed += 10;
+    console.log(`${this.make} is now going at ${this.speed} km/h`);
+  }
+
+  brake(speed) {
+    this.speed -= 10;
+    console.log(`${this.make} is now going at ${this.speed} km/h`);
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+// CONVERTING CONSTRUCTOR FUNCTION TO CLASS
+class EV extends Carcl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeto) {
+    this.charge = chargeto;
+    return this;
+  }
+
+  acclerate(speed) {
+    this.speed += 20;
+    this.charge--;
+    console.log(`${this.make} is now going at ${this.speed} km/h`);
+    return this;
+  }
+}
+
+const Rivian = new EV('Rivian', 120, 23);
+console.log(Rivian.chargeBattery(50));
+*/
