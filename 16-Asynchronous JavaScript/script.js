fcca3bdbc20fe06dea0d1fbe19a54c50de6ae29a
Aakash Rao <aakash.codes02@gmail.com>
2022-04-25 16:15:09 +0530

    Callback Hell

--- 16-Asynchronous JavaScript/script.js
+++ 16-Asynchronous JavaScript/script.js
@@ -39,17 +39,9 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////////////////
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
-
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
-    const html = `
-  <article class="country">
+const renderCountry = (data, className = '') => {
+  const html = `
+  <article class="country ${className}">
     <img class="country__img" src="${data.flags.png}" />
     <div class="country__data">
       <h3 class="country__name">${data.name.common}</h3>
@@ -66,29 +58,51 @@ const getCountryData = function (country) {
     </div>
   </article>
   `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call contry 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+    if (!neighbour) return;
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('India');
-getCountryData('Pakistan');
-// getCountryData('Poland');
-// getCountryData('Germany');
-// getCountryData('France');
-// getCountryData('usa');
-// getCountryData('uk');
-// getCountryData('china');
-// getCountryData('russia');
-// getCountryData('japan');
-// getCountryData('malaysia');
-// getCountryData('indonesia');
-// getCountryData('australia');
-// getCountryData('new zealand');
-// getCountryData('canada');
-// getCountryData('switzerland');
-// getCountryData('sweden');
-// getCountryData('norway');
-// getCountryData('finland');
-// getCountryData('denmark');
-// getCountryData('iceland');
+getCountryAndNeighbour('usa');
+
+// setTimeout(() => {
+//   console.log('1 sec passed!!');
+//   setTimeout(() => {
+//     console.log('2 sec passed!!');
+//     setTimeout(() => {
+//       console.log('3 sec passed!!');
+//       setTimeout(() => {
+//         console.log('4 sec passed!!');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
